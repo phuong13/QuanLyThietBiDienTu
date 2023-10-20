e7900e63900dad77e51f4396d28c7f1cb6eebfec
HPhng <nguyenhoangphuong1132@gmail.com>
2023-10-20 15:28:14 +0700

    F

--- QuanLyThietBiDienTu/Form1.Designer.cs
+++ QuanLyThietBiDienTu/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.homeButton.Location = new System.Drawing.Point(1, 0);
             this.homeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.homeButton.Name = "homeButton";
-            this.homeButton.Size = new System.Drawing.Size(128, 37);
+            this.homeButton.Size = new System.Drawing.Size(144, 46);
             this.homeButton.TabIndex = 0;
             this.homeButton.Text = "Home";
             this.homeButton.UseVisualStyleBackColor = true;
@@ -48,10 +48,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(135, 0);
+            this.button1.Location = new System.Drawing.Point(152, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.Size = new System.Drawing.Size(144, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Kho";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,19 +59,19 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(269, 0);
+            this.button2.Location = new System.Drawing.Point(303, 0);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 37);
+            this.button2.Size = new System.Drawing.Size(144, 46);
             this.button2.TabIndex = 2;
             this.button2.Text = "Hóa Đơn";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 466);
+            this.ClientSize = new System.Drawing.Size(1169, 611);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.homeButton);
